8e627e6603aca36862c83d3ba0d6378d539ff83f
david <david.ouellette12@protonmail.com>
2022-05-24 11:03:43 -0400

    commit on master

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,35 @@
-const getData = () => {
-    fetch('../data.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
+let response = await fetch('../data.json')
+let json = await response.json()
+
+const formatData = (data) => {
+    let total = 0
+    data.map((item, index) => {
+        total = total + item.amount
+        let totalElement = document.getElementById("total")
+        totalElement.innerHTML = "$" + total.toString()
+        let day = document.getElementById(item.day)
+        let amount = item.amount
+        let percentage = amount*1.5
+        if (percentage > 100) percentage = 100;
+        percentage = percentage.toString() + 'px'
+        day.style.height = percentage;
     })
 }
 
-getData()
+formatData(json)
+
+const bars = document.querySelectorAll(".trend__graph-bar")
+
+bars.forEach(element => {
+    element.addEventListener("mouseover", (event) => {
+        let tag = element.querySelector('#' + event.target.id.toString() + '-tag')
+        json.map((item) => {
+            if (item.day === event.target.id) tag.innerHTML = '$' + item.amount.toString()
+        })
+        tag.classList.add('trend__amount-tag--visible')
+        })
+    element.addEventListener("mouseout", (event) => {
+        let tag = element.querySelector('#' + event.target.id.toString() + '-tag')
+        tag.classList.remove('trend__amount-tag--visible')
+    })
+});
